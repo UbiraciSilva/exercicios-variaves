6090ab05bf6adf8a5b3cce248205aeac92e25fb3
Ubiraci Silva <estagio.ubiraci@gmail.com>
2024-04-01 12:41:33 -0300

    Adicionado recebimento do número a ser calculado direto com o usuário

--- ExerciLogicaOneAbril/Program.cs
+++ ExerciLogicaOneAbril/Program.cs
@@ -1,14 +1,22 @@
-﻿// Variáves dicas:
+﻿// Dicas de Variáves e como usalas da melhor forma :
 
 int Idade = 26;
 string Nome = "Val";
 string Nome2 = "Olá meu nome é: ";
-var Salario = 16900;
+Console.WriteLine("Olá meu nome e, " + Nome + " " + Idade + " Anos");
+
+var Salario = 16.900f;
+Console.WriteLine($"{Nome2} {Nome } tenho  {Idade} Anos ganho R$ {Salario}");
+
 var Calcular = 18;
-      var CaLcular2  = 17;
-Console.WriteLine("Olá meu nome e, " + Nome +  " Anos " + Idade);
-            Console.WriteLine($"{Nome2} {Nome } tenho  {Idade} Anos ganho R$ {Salario}");
-Console.WriteLine($"Resultado é: {Calcular * CaLcular2}");
+var CaLcular2  = 17;
+Console.WriteLine($"Resultado é: {Calcular + CaLcular2}");
+
+var CaLcular3 = Console.ReadLine();
+var CaLcular4 = Console.ReadLine();
+
+Console.WriteLine(int.Parse(CaLcular3) - int.Parse (CaLcular4));
+
 
 
 
