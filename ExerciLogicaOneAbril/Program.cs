856f2408be4fd9d8ff43c82c455bff5f195abc27
Ubiraci Silva <estagio.ubiraci@gmail.com>
2024-04-01 10:32:35 -0300

    Exercicio de fixação Variáveis, logica e expressão

---
+++ ExerciLogicaOneAbril/Program.cs
@@ -0,0 +1,15 @@
+﻿// Variáves dicas:
+
+int Idade = 26;
+string Nome = "Val";
+string Nome2 = "Olá meu nome é: ";
+var Salario = 16900;
+var Calcular = 18;
+      var CaLcular2  = 17;
+Console.WriteLine("Olá meu nome e, " + Nome +  " Anos " + Idade);
+            Console.WriteLine($"{Nome2} {Nome } tenho  {Idade} Anos ganho R$ {Salario}");
+Console.WriteLine($"Resultado é: {Calcular * CaLcular2}");
+
+
+
+
